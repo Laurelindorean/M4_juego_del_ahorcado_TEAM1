209f7788eb294746415b396167f9c003896da5e1
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-04-04 09:47:40 +0200

    Ahorcado V1

--- M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/AhorcadoMain.java
+++ M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/AhorcadoMain.java
@@ -5,20 +5,43 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
 
 public class AhorcadoMain extends JFrame {
 
 	private JPanel contentPane;
 
 	public AhorcadoMain() {
+		setTitle("Juego del ahorcado!");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 647, 722);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(319, 11, 302, 661);
+		contentPane.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_1.setBounds(10, 11, 299, 147);
+		contentPane.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_2.setBounds(10, 169, 299, 220);
+		contentPane.add(panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_3.setBounds(10, 400, 299, 272);
+		contentPane.add(panel_3);
 		
 		setVisible(true);
 	}
-
 }

bf9edede6d1e9c2746f29939346bdd83c0d77d5c
Palmira Romia <kguinera@hotmail.com>
2023-04-04 09:16:31 +0200

    primer commit

---
+++ M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/App.java
@@ -0,0 +1,13 @@
+package bootcamp.M4.Team1_Juego_del_ahorcado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

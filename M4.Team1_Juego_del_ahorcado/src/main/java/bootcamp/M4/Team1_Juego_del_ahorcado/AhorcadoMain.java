342939d8f96a99a7e71a521c919451e19ffae19e
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-04 13:47:36 +0200

    Bug Position Fixed

--- M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/AhorcadoMain.java
+++ M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/AhorcadoMain.java
@@ -44,7 +44,7 @@ public class AhorcadoMain extends JFrame implements ActionListener {
 	public AhorcadoMain() {
 		setTitle("Juego del ahorcado!");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 647, 722);
+		setBounds(300, 100, 647, 722);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 

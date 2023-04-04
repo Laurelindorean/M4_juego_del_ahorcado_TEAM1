8cfb1f5d726942a6cda1f5358bd2e447fded453b
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-04 12:07:27 +0200

    AMenu prepared

--- M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/AhorcadoMain.java
+++ M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/AhorcadoMain.java
@@ -10,10 +10,16 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AhorcadoMain extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnNuevoJuego;
+	private JButton btnDificultad;
+	private JButton btnResolver;
+	private JButton btnMasPalabras;
 
 	public AhorcadoMain() {
 		setTitle("Juego del ahorcado!");
@@ -24,49 +30,74 @@ public class AhorcadoMain extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBounds(319, 11, 302, 661);
 		contentPane.add(panel);
-		
+
+		// -----------PANEL DE MENU----------------
 		JPanel panel_menu = new JPanel();
 		panel_menu.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_menu.setBounds(10, 11, 299, 147);
-		contentPane.add(panel_menu);
 		panel_menu.setLayout(null);
-		
-		JButton btnNuevoJuego = new JButton("Nuevo juego");
-		btnNuevoJuego.setBounds(15, 50, 110, 30);
-		panel_menu.add(btnNuevoJuego);
+		contentPane.add(panel_menu);
 		
 		JLabel lblNewLabel = new JLabel("MENU");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		lblNewLabel.setBounds(125, 15, 50, 25);
 		panel_menu.add(lblNewLabel);
-		
-		JButton btnDificultad = new JButton("Dificultad");
+
+		//BOTONES
+		btnNuevoJuego = new JButton("Nuevo juego");
+		btnNuevoJuego.addActionListener(btnsMenu);
+		btnNuevoJuego.setBounds(15, 50, 110, 30);
+		panel_menu.add(btnNuevoJuego);
+
+		btnDificultad = new JButton("Dificultad");
+		btnDificultad.addActionListener(btnsMenu);
 		btnDificultad.setBounds(140, 51, 110, 30);
 		panel_menu.add(btnDificultad);
-		
-		JButton btnResolver = new JButton("Resolver");
+
+		btnResolver = new JButton("Resolver");
+		btnResolver.addActionListener(btnsMenu);
 		btnResolver.setBounds(15, 90, 110, 30);
 		panel_menu.add(btnResolver);
-		
-		JButton btnNewButton_1_1_1 = new JButton("Mas Palabras");
-		btnNewButton_1_1_1.setBounds(140, 90, 110, 30);
-		panel_menu.add(btnNewButton_1_1_1);
-		
+
+		btnMasPalabras = new JButton("Mas Palabras");
+		btnMasPalabras.addActionListener(btnsMenu);
+		btnMasPalabras.setBounds(140, 90, 110, 30);
+		panel_menu.add(btnMasPalabras);
+		// -----------------------------------------------------------------------
+
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_2.setBounds(10, 169, 299, 177);
 		contentPane.add(panel_2);
-		
+
 		JPanel panel_3 = new JPanel();
 		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_3.setBounds(10, 357, 299, 315);
 		contentPane.add(panel_3);
-		
+
 		setVisible(true);
 	}
+
+	// ACTION LISTENER PARA EL MENU
+	ActionListener btnsMenu = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == btnNuevoJuego) {
+
+			}
+			if (e.getSource() == btnDificultad) {
+				AhorcadoWelcome w = new AhorcadoWelcome();
+			}
+			if (e.getSource() == btnResolver) {
+
+			}
+			if (e.getSource() == btnMasPalabras) {
+
+			}
+		}
+	};
 }

c262a0953c1bc6d0909113214e14d1055ed5f1aa
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-04-04 16:00:40 +0200

    funciones update

--- M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/utils/Ahorcado.java
+++ M4.Team1_Juego_del_ahorcado/src/main/java/bootcamp/M4/Team1_Juego_del_ahorcado/utils/Ahorcado.java
@@ -2,10 +2,8 @@ package bootcamp.M4.Team1_Juego_del_ahorcado.utils;
 
 import java.util.ArrayList;
 
-
-
 public class Ahorcado {
-	
+
 	// Comprueba si x carácter está presente en una palabra
 	public static boolean checkLetterInWord(char l, String word) {
 		for (int i = 0; i < word.length(); i++) {
@@ -31,11 +29,26 @@ public class Ahorcado {
 		}
 		return sb.toString();
 	}
-	
-	/*
+
+	// Funcion que actualiza la palabra quitando censura
 	public static String updateWord(String wordOriginal, String wordCensured, char l) {
-		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < wordOriginal.length(); i++) {
+			if (wordOriginal.charAt(i) == ' ') {
+				sb.append(' ');
+			} else if (wordOriginal.charAt(i) == l) {
+				sb.append(l);
+			} else if (wordOriginal.charAt(i) == wordCensured.charAt(i*2)) {
+				sb.append(wordOriginal.charAt(i));
+			}
+			else {
+				sb.append('_');
+			}
+			if (i + 1 < wordOriginal.length()) {
+				sb.append(' ');
+			}
+		}
+		return sb.toString();
 	}
-	*/
 
 }
